3337fd61bd3fedee72b5796b5a37e5c44ae605cc
Pyrofanis <nasos.karachalios.stagas@gmail.com>
2020-05-02 19:51:27 +0300

    uiATworks

---
+++ Assets/Scripts/bambooCollectorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class bambooCollectorScript : MonoBehaviour
+{
+    public GameObject bambooTextobj;
+    public int bambooscore=0,bambootoupgrade=70;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag =="bamboo")
+        {
+            bambooscore++;
+            bambooTextobj.GetComponent<TMPro.TextMeshProUGUI>().text = bambooscore.ToString() +  "/" + bambootoupgrade.ToString();
+            collision.gameObject.SetActive(false);
+        }
+        Debug.Log("KOLAI");
+    }
+
+}
