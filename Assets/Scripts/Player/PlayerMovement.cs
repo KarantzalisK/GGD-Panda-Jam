3f099c062b5564aec7b67d644bd61c98ef0438f4
nasos karachalios stagkas <nasos.karachalios.stagas@gmail.com>
2020-05-15 04:43:29 +0300

    MovementScriptPolishing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movementOFplayer;
     private float speed;
     [HideInInspector]
     public  Vector2 movement;
