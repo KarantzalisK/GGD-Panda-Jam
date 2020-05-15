761d755de3a3e7df83b46c605173ac12dab14b9f
nasos karachalios stagkas <nasos.karachalios.stagas@gmail.com>
2020-05-15 07:05:07 +0300

    FirstTweeingInitiatedPLUSUpgrades

---
+++ Assets/Scripts/UiUpgrades/UpgradesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UpgradesController : MonoBehaviour
+{
+    private uIUpgradesAnimations animationControlls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationControlls = GetComponent<uIUpgradesAnimations>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActivatePanel();
+    }
+
+    private void ActivatePanel()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&!animationControlls.CanAnimate)
+        {
+            animationControlls.CanAnimate = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)&& animationControlls.CanAnimate)
+        { 
+            animationControlls.CanAnimate = false; }
+    }
+}
---
+++ Assets/Scripts/UiUpgrades/uIUpgradesAnimations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class uIUpgradesAnimations : MonoBehaviour
+{   [HideInInspector]
+    public bool CanAnimate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanAnimate)
+        {
+            gameObject.GetComponent<Transform>().DOScaleY(1,1f);
+        }
+        else {
+            gameObject.GetComponent<Transform>().DOScaleY(0, 0.5f);
+        }
+    }
+}
