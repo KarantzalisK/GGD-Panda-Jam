1632ebf68dde27bf4bb5e4380a47bac38cd320cf
nasos karachalios stagkas <nasos.karachalios.stagas@gmail.com>
2020-06-18 14:41:33 +0300

    SmallReworks/WinningPanel/AddedAdoor

---
+++ Assets/Scripts/enemy/WinningConditionsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class WinningConditionsScript : MonoBehaviour
+{
+    private SpawnManager spawnManager;
+    public GameObject winingPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManager = GetComponent<SpawnManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WinIfMaxWaveReachedAndNoEnemiesInScene();
+    }
+    private void WinIfMaxWaveReachedAndNoEnemiesInScene()
+    {
+        if (spawnManager.waveNumber==spawnManager.activeWave.Count)//lastWaveISEmpty
+        {
+            winingPanel.SetActive(true);
+        }
+    }
+}
